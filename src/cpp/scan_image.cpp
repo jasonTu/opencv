dd6b35e945eb55a4f8f3e02df68c45d2c399205f
Jason Tu <jason_tu@puyacn.com>
2016-12-12 14:35:02 +0800

    [imp] How to scan images, lookup tables and time measurement with OpenCV

---
+++ src/cpp/scan_image.cpp
@@ -0,0 +1,181 @@
+/**
+ * The example code descript image scan process.
+ * Author: Jason Tu
+ * You can refer to the following materials:
+ * http://docs.opencv.org/2.4/doc/tutorials/core/how_to_scan_images/how_to_scan_images.html#howtoscanimagesopencv
+ * http://docs.opencv.org/2.4/_downloads/how_to_scan_images.cpp
+ */
+#include <iostream>
+#include <sstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+Mat& ScanImageAndReduceC(Mat& I, const uchar* table);
+Mat& ScanImageAndReduceIterator(Mat& I, const uchar* table);
+Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar * table);
+
+int main()
+{
+    Mat I, J;
+    I = imread("img/dota.jpg", CV_LOAD_IMAGE_COLOR);
+
+    if (!I.data) {
+        cout << "The image could not be loaded." << endl;
+        return -1;
+    }
+
+    int divideWith = 10; // Convert our input string to number - C++ style
+
+    uchar table[256];
+    for (int i = 0; i < 256; ++i) {
+        table[i] = (uchar)(divideWith * (i / divideWith));
+    }
+
+    const int times = 100;
+    double t;
+
+    t = (double)getTickCount();
+
+    for (int i = 0; i < times; ++i) {
+        Mat clone_i = I.clone();
+        J = ScanImageAndReduceC(clone_i, table);
+    }
+
+    t = 1000 * ((double)getTickCount() - t) / getTickFrequency();
+    t /= times;
+
+    cout << "Time of reducing with the C operator [] (averaged for "
+        << times << " runs): " << t << " milliseconds." << endl;
+
+    t = (double)getTickCount();
+
+    for (int i = 0; i < times; ++i) {
+        Mat clone_i = I.clone();
+        J = ScanImageAndReduceIterator(clone_i, table);
+    }
+
+    t = 1000 * ((double)getTickCount() - t) / getTickFrequency();
+    t /= times;
+
+    cout << "Time of reducing with the iterator (averaged for "
+        << times << " runs): " << t << " milliseconds." << endl;
+
+    t = (double)getTickCount();
+
+    for (int i = 0; i < times; ++i) {
+        Mat clone_i = I.clone();
+        ScanImageAndReduceRandomAccess(clone_i, table);
+    }
+
+    t = 1000 * ((double)getTickCount() - t) / getTickFrequency();
+    t /= times;
+
+    cout << "Time of reducing with the on-the-fly address generation "
+        << "- at function (averaged for "
+        << times << " runs): " << t << " milliseconds." << endl;
+
+    Mat lookUpTable(1, 256, CV_8U);
+    uchar* p = lookUpTable.data;
+    for (int i = 0; i < 256; ++i) {
+        p[i] = table[i];
+    }
+
+    t = (double)getTickCount();
+
+    for (int i = 0; i < times; ++i) {
+        LUT(I, lookUpTable, J);
+    }
+
+    t = 1000 * ((double)getTickCount() - t) / getTickFrequency();
+    t /= times;
+
+    cout << "Time of reducing with the LUT function (averaged for "
+        << times << " runs): " << t << " milliseconds." << endl;
+    return 0;
+}
+
+Mat& ScanImageAndReduceC(Mat& I, const uchar* const table)
+{
+    // Accept only char type matrices
+    CV_Assert(I.depth() == CV_8U);
+
+    int channels = I.channels();
+
+    int nRows = I.rows;
+    int nCols = I.cols * channels;
+
+    if (I.isContinuous()) {
+        nCols *= nRows;
+        nRows = 1;
+    }
+
+    int i, j;
+    uchar* p;
+    for (i = 0; i < nRows; ++i) {
+        p = I.ptr<uchar>(i);
+        for (j = 0; j < nCols; ++j) {
+            p[j] = table[p[j]];
+        }
+    }
+    return I;
+}
+
+Mat& ScanImageAndReduceIterator(Mat& I, const uchar* const table)
+{
+    // Accept only char type matrices
+    CV_Assert(I.depth() == CV_8U);
+
+    const int channels = I.channels();
+    switch (channels) {
+    case 1: {
+        MatIterator_<uchar> it, end;
+        for (it = I.begin<uchar>(), end = I.end<uchar>(); it != end; ++it)
+            *it = table[*it];
+        break;
+    }
+    case 3: {
+        MatIterator_<Vec3b> it, end;
+        for (it = I.begin<Vec3b>(), end = I.end<Vec3b>(); it != end; ++it) {
+            (*it)[0] = table[(*it)[0]];
+            (*it)[1] = table[(*it)[1]];
+            (*it)[2] = table[(*it)[2]];
+        }
+    }}
+
+    return I;
+}
+
+Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* const table)
+{
+    // Accept only char type matrices
+    CV_Assert(I.depth() == CV_8U);
+
+    const int channels = I.channels();
+    switch (channels) {
+    case 1: {
+        for (int i = 0; i < I.rows; ++i) {
+            for (int j = 0; j < I.cols; ++j) {
+                I.at<uchar>(i, j) = table[I.at<uchar>(i, j)];
+            }
+        }
+        break;
+    }
+    case 3: {
+        Mat_<Vec3b> _I = I;
+
+        for (int i = 0; i < I.rows; ++i) {
+            for (int j = 0; j < I.cols; ++j) {
+                _I(i, j)[0] = table[_I(i, j)[0]];
+                _I(i, j)[1] = table[_I(i, j)[1]];
+                _I(i, j)[2] = table[_I(i, j)[2]];
+            }
+        }
+        I = _I;
+        break;
+    }}
+
+    return I;
+}
